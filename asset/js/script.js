c5dc0d0f94230382280d2891460b52d0e4f3e01b
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-21 23:15:38 +0200

    plans js click

--- asset/js/script.js
+++ asset/js/script.js
@@ -7,7 +7,17 @@ $(function() {
     $('.drop-help').click(function() {
         $('.help-menu').toggleClass('active');
     })
+
+
     $('.drop-abaut').click(function() {
         $('.abaut-menu').toggleClass('active');
     })
+
+    $('.drop-plans').click(function() {
+        $('.plans-menu').toggleClass('active');
+    })
+
+
+
+
 })
